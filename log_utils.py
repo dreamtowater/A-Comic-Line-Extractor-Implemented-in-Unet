7d6cd61559c11a904e2595da0dc05857ac687436
dreamtowater <113572549+dreamtowater@users.noreply.github.com>
2024-05-28 20:18:09 +0800

    Update log_utils.py

--- log_utils.py
+++ log_utils.py
@@ -7,15 +7,6 @@ from termcolor import colored
 
 @functools.lru_cache()
 def create_logger(name, log_path):
-    """
-    name: logger名,建议以device命名,如'cpu'、'cuda:0'。
-    log_path: 日志文件路径,文件名也建议以device命名,但要替换非法字符。
-
-    注意:
-    在ipynb文件中每次调用该函数前需重启内核,否则会重复输出。
-    (因为logging.getLogger只有在内存中找不到logger时才新建logger,addHandler会重复执行)
-    若无logger.addHandler(console_handler)则不在控制台输出,多卡时可设置成只有主卡才在控制台输出。
-    """
 
     # create logger
     logger = logging.getLogger(name)
